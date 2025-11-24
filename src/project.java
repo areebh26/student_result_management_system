44659c30174ae0573cd2c40bbd442f2fb496ba0b
areeb <areebulhaq2005@gmail.com>
2025-11-24 20:34:30 +0500

    created Course class

--- src/project.java
+++ src/project.java
@@ -34,7 +34,70 @@
                  "\nqualification : " + qualification;
      }
  }
+class Course{
+    private String courseID;
+    private String title;
+    private int creditHours;
+    private CourseInstructor instructor;
+    private static int totalCourse=0;
 
+    public Course(String courseID, String title, int creditHours, CourseInstructor instructor) {
+        this.courseID = courseID;
+        this.title = title;
+        this.creditHours = creditHours;
+        this.instructor = instructor;
+        totalCourse++;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    public CourseInstructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(CourseInstructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public static int getTotalCourses() {
+        return totalCourses;
+    }
+
+    public static void setTotalCourses(int totalCourses) {
+        Course.totalCourses = totalCourses;
+    }
+
+    @Override
+    public String toString() {
+        return "Course : " +
+                "\nID : " + courseID  +
+                "\ntitle : " + title +
+                "\ncreditHours : " + creditHours +
+                "\ninstructor : " + instructor.getName();
+    }
+}
 
 
 
