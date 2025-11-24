985aa5866df36a45c7b3d0aa43e86416cabc65c6
areeb <areebulhaq2005@gmail.com>
2025-11-24 20:15:27 +0500

    created CourseInstructor class and its attributes .

--- src/project.java
+++ src/project.java
@@ -1,3 +1,39 @@
+  class CourseInstructor{
+    private String name;
+    private String qualification;
+
+    public CourseInstructor(String name, String qualification) {
+         this.name = name;
+         this.qualification = qualification;
+    }
+    public CourseInstructor() {
+         this.name = "tanveer";
+         this.qualification = "phd in Mathematics";
+    }
+
+     public String getName() {
+         return name;
+     }
+
+     public void setName(String name) {
+         this.name = name;
+     }
+
+     public String getQualification() {
+         return qualification;
+     }
+
+     public void setQualification(String qualification) {
+         this.qualification = qualification;
+     }
+
+     @Override
+     public String toString() {
+         return "Instructor : " +
+                 "\nname : " + name  +
+                 "\nqualification : " + qualification;
+     }
+ }
 
 
 
